7309f3bb662ef20528cddf1e2f5a8c057a9295eb
Miguel <miguel@redradix.com>
2017-12-19 17:31:45 +0100

    steps for solution

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Sustituir filter y map por sus versiones de ramda
 
+// 1. Functions as first order citizens
+// 2. Combining functions
+
 export default function projectsForCompany(projects, company) {
   const filtered = projects.filter(project => project.company === company);
   return filtered.map(project => project.name);
 }
+
+// replace map filter
+// compose/pipe
+// partial application/curry
+// imperative vs functional. all operator replacements
+// point free programming
+// lenses
